df3eb98c600fb3a3434e8d99f8498d41b846c5b5
jtubtimt <jirayu@hotmail.de>
2022-05-26 12:56:24 +0700

    abc

--- ft_memmove.c
+++ ft_memmove.c
@@ -14,40 +14,26 @@
 /*
 #include <stdio.h>
 */
-void	*src_desc(unsigned char *dest, unsigned char *src, size_t i, size_t n)
-{
-	while (n - 1 <= i)
-	{
-		dest[n] = (unsigned char)src[n];
-		n--;
-	}
-	return (dest);
-}
-
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t			i;
 	unsigned char	*tmp_dest;
 	unsigned char	*tmp_src;
 
 	tmp_dest = (unsigned char *)dest;
 	tmp_src = (unsigned char *)src;
-	i = 0;
 	if ((n == 0) || (dest == src))
 		return (dest);
 	if (!dest && !src)
 		return (0);
-	if (src < dest)
+	if (tmp_src < tmp_dest)
 	{
-		tmp_dest = src_desc(tmp_dest, tmp_src, i, n);
-		return (tmp_dest);
+		while (n--)
+			*(tmp_dest + n) = *(tmp_src + n);
+		return (dest);
 	}
-	while (i < n)
-	{
-		tmp_dest[i] = (unsigned char)tmp_src[i];
-		i++;
-	}
-	return (tmp_dest);
+	while (n--)
+		*tmp_dest++ = *tmp_src++;
+	return (dest);
 }
 /*
 int main()
